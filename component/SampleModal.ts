d98efb4a9ab6f3f2bf3db6676081f0a70a52dd5a
Luy9696 <dnrdydsla@gmail.com>
2025-01-10 23:46:55 +0900

    테스트 태그 리스트 불러오기

--- component/SampleModal.ts
+++ component/SampleModal.ts
@@ -1,13 +1,39 @@
-import { Modal, App } from "obsidian";
+import { Modal, App, TFile } from "obsidian";
 
 export class SampleModal extends Modal {
+  tags: Set<string>;
+
   constructor(app: App) {
     super(app);
+    this.tags = new Set();
+    this.getAllTags();
+  }
+
+  getAllTags() {
+    const { vault, metadataCache } = this.app;
+    console.log(vault,metadataCache)
+
+    vault.getMarkdownFiles().forEach((file: TFile) => {
+      const cache = metadataCache.getFileCache(file);
+      console.log(cache)
+      if (cache?.tags) {
+        cache.tags.forEach((tag) => this.tags.add(tag.tag));
+      }
+    });
   }
 
   onOpen() {
     const { contentEl } = this;
-    contentEl.setText("s!");
+
+    contentEl.createEl("h2", { text: "All Tags in Vault" });
+    if (this.tags.size === 0) {
+      contentEl.createEl("p", { text: "No tags found in the vault." });
+    } else {
+      const ul = contentEl.createEl("ul");
+      this.tags.forEach((tag) => {
+        ul.createEl("li", { text: tag });
+      });
+    }
   }
 
   onClose() {
